43d64e0aa7bc171db531b920bc9141281b572e94
koushikkumarkadari <139983437+koushikkumarkadari@users.noreply.github.com>
2025-03-23 22:12:16 +0530

    refactor: update Todo model to use ES6 module syntax

--- server/models/Todo.js
+++ server/models/Todo.js
@@ -1,20 +1,19 @@
-const mongoose=require("mongoose")
+import mongoose from "mongoose";
 
-
-const TodoSchema=new mongoose.Schema({
-    newTodo:String,
-    done:{
-        type:Boolean,
-        default:false
+const TodoSchema = new mongoose.Schema({
+    newTodo: String,
+    done: {
+        type: Boolean,
+        default: false
     },
-})
+});
 
 // User Schema
 const userSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    todoList:[TodoSchema]
-  });
-  
+    todoList: [TodoSchema]
+});
+
 const User = mongoose.model('User', userSchema);
-module.exports=User
+export default User;
